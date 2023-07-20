8b64933298924efb77141da3a7dd4ce285250b21
kobid <upadhayakobid6@gmail.com>
2023-07-20 10:21:32 +0530

    added navigation and shared layout for home and starred pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
+
 
 const  Home = () =>{
-return<div> 
-<Link to="/starred"> Go to starred pages</Link>
-
+return (
+<div> 
+Home
 </div>
+);
 };
 
 export default Home;
