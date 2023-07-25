e37b7c7f46fc7754f1924c18b60d0a2ab854779a
kobid <upadhayakobid6@gmail.com>
2023-07-25 19:47:26 +0530

    fetched starred shows data and display showgrid for them

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+
 const BASE_URL = 'https://api.tvmaze.com';
 
 const apiGet = async queryString => {
@@ -11,3 +13,10 @@ export const searchForShows = query => apiGet(`/search/shows?q=${query}`);
 export const searchForpeople = query => apiGet(`/search/people?q=${query}`);
 
 export const getShowById =(showId)=> apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`);
+
+export const getShowByIds = async showIds=> {
+  const Promises = showIds.map(showId => apiGet(`/shows/${showId}`));
+   
+   return Promise.all(Promises);
+
+};
